2cd2c4f1e6ed7c6b2db3fdbbb810fe7e56ba294a
Kyle Glover <kyle.glover524@gmail.com>
2015-08-12 18:21:20 +1000

    Merged 'Tile.j' & 'World.j'

--- src/java/client/world/World.java
+++ src/java/client/world/World.java
@@ -1,29 +1,83 @@
 package java.client.world;
 
+/**
+ * 'World' provides and manages information about all the tiles in the game that
+ * are processed by the 'MonsterGame' program.
+ * 
+ * Things that need to be coded: 
+ * - Apply textures in 'changeTileState'
+ * - Create the updatePos method
+ */
 public class World {
+
+	// Create possible states of a tile
+	public enum Tile { EMPTY, WALL, BLOCKED };
 	
-	// Create Tile Array
-	Tile[][] tiles;
-	
-	protected World (int a) {
-		
-		// Initialise Tile Array
-		this.tiles = new Tile[a][a];
+	Tile[][] tiles = new Tile[2][2];
+
+	public World(int a) {
+
 	}
-	
+
+	/**
+	 * Change the state of a tile by returning a given state Exits if state
+	 * could not be changed
+	 * 
+	 * @param newState
+	 * @return
+	 */
+	public Tile changeTileState(Tile newState) {
+		switch (newState) {
+		case EMPTY:
+			// ** Apply 'EMPTY' texture here ** //
+
+			// Return tile is EMPTY
+			return Tile.EMPTY;
+
+		case WALL:
+			// ** Apply 'WALL' texture here ** //
+
+			// Return tile is WALL
+			return Tile.WALL;
+
+		case BLOCKED:
+			// ** Apply correct texture here ** //
+
+			// Return tile is BLOCKED
+			return Tile.BLOCKED;
+
+		default:
+			// Output invalid state
+			System.out.println("Invalid tile state: " + newState);
+			System.out.println("Exiting.");
+
+			// Exit the system
+			System.exit(0);
+		}
+
+		// This is for eclipse
+		return null;
+	}
+
+	/**
+	 * Is the tile accessible to the player/robot
+	 * 
+	 * @param tile
+	 * @return
+	 */
+	public boolean isAccessible(Tile tile) {
+		return tile != Tile.EMPTY;
+	}
+
 	public void updatePosition(Player player, int[] oldPosition, int[] newPosition) {
-		
+
 		// Get player number
 		// Select player
 		// Get newPosition
-			// Check new position isn't occupied by PLAYER/WALL/ROBOT
+		// Check new position isn't occupied by PLAYER/WALL/ROBOT
 		// Set newPosition to PLAYER if passes checks
-		
+
 		// Get oldPosition, set oldPosition to EMPTY
 	}
-	
-	
-	public void setWalls(Tile[][] firstPos, Tile[][] secondPos) {
-		// Sets a specific region to WALL
-	}
+
 }
