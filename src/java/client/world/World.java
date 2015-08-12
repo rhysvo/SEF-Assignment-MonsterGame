98c2df0855865f3eb4d1e5b72ba50bb2b652fefd
Kyle Glover <kyle.glover524@gmail.com>
2015-08-12 11:20:58 +1000

    Added updatePos & setWalls with descriptions

--- src/java/client/world/World.java
+++ src/java/client/world/World.java
@@ -13,5 +13,17 @@ public class World {
 	
 	public void updatePosition(Player player, int[] oldPosition, int[] newPosition) {
 		
+		// Get player number
+		// Select player
+		// Get newPosition
+			// Check new position isn't occupied by PLAYER/WALL/ROBOT
+		// Set newPosition to PLAYER if passes checks
+		
+		// Get oldPosition, set oldPosition to EMPTY
+	}
+	
+	
+	public void setWalls(Tile[][] firstPos, Tile[][] secondPos) {
+		// Sets a specific region to WALL
 	}
 }
