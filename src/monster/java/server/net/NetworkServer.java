8c0cb110912420d855936c2895384677c2adbb2e
Alex-laptop <amatho250@gmail.com>
2015-09-07 17:34:54 +1000

    Work on initial connections

--- src/monster/java/server/net/NetworkServer.java
+++ src/monster/java/server/net/NetworkServer.java
@@ -1,5 +1,52 @@
 package monster.java.server.net;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
+import monster.java.server.world.NetworkPlayer;
+
 public class NetworkServer {
 
+	private int port;
+	private ServerSocket serverSocket;
+	private ArrayList<NetworkPlayer> players;
+	private int readyPlayers;
+	
+	public NetworkServer(int port) {
+		this.port = port;
+	}
+	
+	public void addReady() {
+		this.readyPlayers++;
+	}
+	
+	public void init() {
+		try {
+			this.serverSocket = new ServerSocket(this.port);
+			
+			System.out.println("Waiting for players on port " + this.port);
+			
+			int i = 0;
+			while (this.readyPlayers < this.players.size() && (i = this.players.size()) < 4) {
+				
+				this.players.add(new NetworkPlayer(this.serverSocket.accept()));
+				this.players.get(i).send("player:" + i);
+				
+			}
+			
+		} catch (IOException e) {
+			
+			System.out.println("Error connecting to players.");
+			e.printStackTrace();
+			
+		}
+	}
+	
+	public void run() {
+		
+		
+
+	}
+	
 }
--- src/monster/java/server/world/NetworkPlayer.java
+++ src/monster/java/server/world/NetworkPlayer.java
@@ -1,5 +1,57 @@
 package monster.java.server.world;
 
-public class NetworkPlayer {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import monster.java.server.MonsterServer;
+
+public class NetworkPlayer extends Thread {
+	
+	private Socket socket;
+	private PrintWriter out;
+	private BufferedReader in;
+	private boolean isReady = false;
+	
+	public NetworkPlayer(Socket socket) {
+		this.socket = socket;
+		
+		System.out.println("New player connected to " + socket.getInetAddress() + " " + socket.getPort());
+		
+		try {
+			this.out = new PrintWriter(socket.getOutputStream(), true);
+			this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		} catch (IOException e) {
+			System.out.println("Unable to connect to player streams.");
+			e.printStackTrace();
+		}
+		
+		// Begin the thread
+		this.start();
+	}
+	
+	public void send(String msg) {
+		this.out.println(msg);
+	}
+	
+	public void run() {
+		String inputLine;
+		try {
+			while ((inputLine = this.in.readLine()) != null) {
+				System.out.println(inputLine);
+	
+				// TODO: handle incoming messages
+				if (inputLine == "ready" && !this.isReady) {
+					this.isReady = true;
+					MonsterServer.server.addReady();
+				}				
+			}
+		} catch (IOException e) {
+			System.out.println("Bad message from client " + this.socket.getInetAddress());
+			e.printStackTrace();
+		}
+	}
 
 }
