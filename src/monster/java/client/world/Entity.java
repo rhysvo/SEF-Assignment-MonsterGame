213a91693ce58cd3175ac7127a5d9106f2873dc8
Hope Underwood <hopeunderwood@Hopes-MacBook-Pro.local>
2015-09-28 20:38:18 +1000

    cleaning up classes, dynamic access. NOT CONNECTING TO SERVER

--- src/monster/java/client/world/Entity.java
+++ src/monster/java/client/world/Entity.java
@@ -1,12 +1,18 @@
 package monster.java.client.world;
 
 import static org.lwjgl.opengl.ARBTextureRectangle.GL_TEXTURE_RECTANGLE_ARB;
-import static org.lwjgl.opengl.GL11.*;
-
-import org.newdawn.slick.opengl.Texture;
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glBindTexture;
+import static org.lwjgl.opengl.GL11.glColor3f;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glPopMatrix;
+import static org.lwjgl.opengl.GL11.glPushMatrix;
+import static org.lwjgl.opengl.GL11.glTexCoord2f;
+import static org.lwjgl.opengl.GL11.glTranslatef;
+import static org.lwjgl.opengl.GL11.glVertex2f;
 
 import monster.java.client.MonsterGame;
-import monster.java.client.game.Game;
 import monster.java.client.net.MessageProtocol;
 import monster.java.client.util.Sprite;
 import monster.java.client.util.TextureLoading;
@@ -78,31 +84,32 @@ public class Entity {
 	}
 	
 	public Sprite getEntityTexture(int player) {
+		TextureLoading entityTexture = MonsterGame.instance.game.getTextureLoading();
 		switch(player) {
 		
 			case 0:
 				// Monster 
-				return Game.spriteMap.get("monster");
+				return entityTexture.getSprite("monster");
 				
 			case 1:
 				// Player 1
-				return Game.spriteMap.get("p1");
+				return entityTexture.getSprite("p1");
 				
 			case 2:
 				// Player 2
-				return Game.spriteMap.get("p2");
+				return entityTexture.getSprite("p2");
 				
 			case 3:
 				// Player 3
-				return Game.spriteMap.get("p3");
+				return entityTexture.getSprite("p3");
 				
 			case 4:
 				// Player 4
-				return Game.spriteMap.get("p4");
+				return entityTexture.getSprite("p4");
 				
 			default:
 				// Player 1 Sprite
-				return Game.spriteMap.get("p1");
+				return entityTexture.getSprite("p1");
 		}
 	}
 	
@@ -111,7 +118,7 @@ public class Entity {
 	public void draw() {
 		float tileSize = (float) MonsterGame.TILE_SIZE;	
 		glPushMatrix();
-		glBindTexture(GL_TEXTURE_RECTANGLE_ARB, Game.spritesheet);
+		glBindTexture(GL_TEXTURE_RECTANGLE_ARB, MonsterGame.instance.game.getTextureLoading().spritesheet);
 		Sprite currentSprite = getEntityTexture(id);
 		
 		int gx = currentSprite.getX();
