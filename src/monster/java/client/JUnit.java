44cdd70a5b7fb44696aa06536c4129d430d58060
Kyle Glover <s3539870@student.rmit.edu.au>
2015-10-15 22:30:17 +1100

    Basic skeleton test cases

---
+++ src/monster/java/client/JUnit.java
@@ -0,0 +1,20 @@
+package monster.java.client;
+
+import org.junit.Test;
+
+public class JUnit {
+	@Test
+	public void playerMove() {
+		// Check that a player can move in all four directions using arrow keys
+	}
+	
+	@Test
+	public void playerUnableToLeaveDomain() {
+		// Verify that a player can not leave the play area
+	}
+	
+	@Test
+	public void noPlayerOverlap() {
+		// Verify that players are unable to overlap (onto each other)
+	}
+}
